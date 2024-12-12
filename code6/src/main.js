c83f01fabedf6bbb79fbe17955204b9060e8123e
farhanghaisani <ghaisanifarhan@gmail.com>
2024-12-12 20:15:05 +0700

    Initial commit for Modul6 branch

--- code6/src/main.js
+++ code6/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+//import createApp from Vue
+import { createApp } from 'vue';
+//import component App
+import App from './App.vue';
+//import config router
+import router from './router'
+//create App Vue
+const app = createApp(App);
+//gunakan "router" di Vue dengan plugin "use"
+app.use(router);
+app.mount('#app');
